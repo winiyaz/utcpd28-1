ae85e3cc2730e48830337fe57f8daf35b31b00b7
winiyaz <winiyaz@proton.me>
2024-08-22 04:08:01 -0700

    🙌 D28Setup - DynamicTying

--- p1/main.py
+++ p1/main.py
@@ -7,9 +7,10 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
@@ -17,7 +18,19 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
-	count_down(5 * 60)
+	global reps
+	reps += 1
+
+	work_sec = WORK_MIN * 60
+	short_break_sec = SHORT_BREAK_MIN * 60
+	long_break_sec = LONG_BREAK_MIN * 60
+
+	if reps % 8 == 0:
+		count_down(long_break_sec)
+	elif reps % 2 == 0:
+		count_down(short_break_sec)
+	else:
+		count_down(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -30,6 +43,8 @@ def count_down(count):
 	canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
 	if count > 0:
 		window.after(1000, count_down, count - 1)
+	else:
+		start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
