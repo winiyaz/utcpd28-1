d76e6b28f1c45b02b9ca5b33c17f9dc98c975020
winiyaz <winiyaz@proton.me>
2024-08-22 02:16:56 -0700

    🙌 D28Setup - StartTimer

--- p1/main.py
+++ p1/main.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -11,18 +10,28 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start_timer():
+	count_down(5)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+def count_down(count):
+	canvas.itemconfig(timer_text, text=count)
+	if count > 0:
+		window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("AssSuckingTimer")
 # window.minsize(width=600, height=600)
 window.configure(bg="#020617", padx=200, pady=100)
-check_mark = "✅"
 
 # Main label
 main_label = Label(text="BootyTimer", fg="yellow", bg="black", font=("Arial", 50, 'bold'))
@@ -31,12 +40,12 @@ main_label.grid(column=1, row=0)
 # -- Canvas ---
 canvas = Canvas(width=400, height=400, bg="#020617", highlightthickness=0)
 girl_img = PhotoImage(file="w.png")
-canvas.create_image(200,200, image=girl_img)
-canvas.create_text(200,175, text="00:00", font=(FONT_NAME, 50, "bold"), fill="#DA0C81")
+canvas.create_image(200, 200, image=girl_img)
+timer_text = canvas.create_text(200, 175, text="00:00", font=(FONT_NAME, 50, "bold"), fill="#DA0C81")
 canvas.grid(column=1, row=1)
 
 # Adding Buttons
-start_button = Button(text="Start", bg="#005B41", fg="white", font=("Arial", 20))
+start_button = Button(text="Start", bg="#005B41", fg="white", font=("Arial", 20), command=start_timer)
 start_button.grid(column=0, row=3, pady=20)
 reset_button = Button(text="Reset", bg="#35155D", fg="white", font=("Arial", 20))
 reset_button.grid(column=2, row=3, pady=20)
